485c107d0db660db6e622a88e3b403f99f87efd2
Harsh Verma <harshshredding@gmail.com>
2022-11-10 18:40:41 -0500

    github

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 alias s="git status -uno"
-alias github="cat ~/github.txt"
+alias github="cat ~/github.txt | clip"
 alias login="ssh h_ver@login.encs.concordia.ca"
 alias lab="ssh -L 3336:adjp:80 h_ver@login.encs.concordia.ca"
 alias gate="gate.sh -Xmx16g"
