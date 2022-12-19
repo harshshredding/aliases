ee7119a706c1c3e962aa02f27a725da0911b5144
Harsh Verma <harshshredding@gmail.com>
2022-12-19 16:49:17 -0500

    mode useful aliases:

--- aliases.sh
+++ aliases.sh
@@ -1,17 +1,27 @@
-
+# compile and run simple c++ code
 function run() {
   g++ "$1" && ./a.out;
 }
 
+# search in history
 function his() {
   history | grep "$1";
+}
+
+# fuzzy version of cd (partial matching with folder)
+function cds() {
+  cd "$(/bin/ls | grep $1)";
+}
+
+function r() {
+  grep -Ril "$1";
 }
 
 alias s="git status -uno"
 alias github="cat ~/github.txt | clip"
 alias login="ssh h_ver@login.encs.concordia.ca"
 alias lab="ssh -L 3336:adjp:80 h_ver@login.encs.concordia.ca"
-alias gate="gate.sh -Xmx16g"
+alias gate="gate.sh -Xmx4g"
 alias l="ls -l --block-size=M"
 alias clip='xclip -sel clip'
 alias ssh_pub='cat ~/.ssh/id_ed25519.pub | clip'
@@ -24,3 +34,12 @@ alias aliases="vim ~/aliases/aliases.sh"
 alias c++="code ~/projects/c++"
 alias bergler="echo bergler@cse.concordia.ca | clip"
 alias book="mupdf ~/Documents/books/code-complete-2nd-edition-v413hav.pdf"
+alias sys_book="mupdf ~/Documents/books/systems.pdf"
+alias g="cd ~/Documents/grading"
+alias m="mupdf"
+alias disable="sudo sysctl net.ipv6.conf.all.disable_ipv6=1"
+alias todo="vim ~/todo.txt"
+alias ls="exa"
+alias every="cd ~/every-single-baseline"
+alias brc="vim ~/.bashrc"
+alias clone="echo 'git clone https://github.com/harshshredding/every-single-baseline.git' | clip"
